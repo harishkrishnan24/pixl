7134e1a6f30132bcd7eea36082b03936263c1f83
Harish Krishnan <harishkrishnan1993@gmail.com>
2022-06-20 21:14:08 +0530

    Show canvas area

--- pixl/pixl.go
+++ pixl/pixl.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"harishkrishnan24/pixl/apptype"
+	"harishkrishnan24/pixl/pxcanvas"
 	"harishkrishnan24/pixl/swatch"
 	"harishkrishnan24/pixl/ui"
 	"image/color"
 
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 )
 
@@ -18,7 +20,18 @@ func main() {
 		SwatchSelected: 0,
 	}
 
+	pixlCanvasConfig := apptype.PxCanvasConfig{
+		DrawingArea:  fyne.NewSize(600, 600),
+		CanvasOffset: fyne.NewPos(0, 0),
+		PxRows:       10,
+		PxCols:       10,
+		PxSize:       30,
+	}
+
+	pixlCanvas := pxcanvas.NewPxCanvas(&state, pixlCanvasConfig)
+
 	appInit := ui.AppInit{
+		PixlCanvas: pixlCanvas,
 		PixlWindow: pixlWindow,
 		State:      &state,
 		Swatches:   make([]*swatch.Swatch, 0, 64),
--- ui/types.go
+++ ui/types.go
@@ -2,12 +2,14 @@ package ui
 
 import (
 	"harishkrishnan24/pixl/apptype"
+	"harishkrishnan24/pixl/pxcanvas"
 	"harishkrishnan24/pixl/swatch"
 
 	"fyne.io/fyne/v2"
 )
 
 type AppInit struct {
+	PixlCanvas *pxcanvas.PxCanvas
 	PixlWindow fyne.Window
 	State      *apptype.State
 	Swatches   []*swatch.Swatch
